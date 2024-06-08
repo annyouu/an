55359437330730a53836c174ff221be5ef835b04
annyouu <165882043+annyouu@users.noreply.github.com>
2024-06-09 01:07:31 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,14 @@
 'use strict';
-
 {
-  function showPassword() {
-    const result = document.getElementById('result');
-    const numbersCheckbox = document.getElementById('numbers-checkbox');
-    const symbolsCheckbox = document.getElementById('symbols-checkbox');
-    const letters = 'abcdefghijklmnopqrstuvwxyz';
-    const numbers = '0123456789';
-    const symbols = '!#$%&?';
-    let password = '';
-    let seed = letters + letters.toUpperCase();
-
-    if (numbersCheckbox.checked) {
-      seed += numbers;
-    }
-
-    if (symbolsCheckbox.checked) {
-      seed += symbols;
-    }
-
-    for (let i = 0; i < slider.value; i++) {
-      password += seed[Math.floor(Math.random() * seed.length)];
-    }
-    
-    result.textContent = password;
-  }
-  const slider = document.getElementById('slider');
   const btn = document.getElementById('btn');
-
-  slider.addEventListener('input', () => {
-    const passwordLength = document.getElementById('password-length');
-    passwordLength.textContent = slider.value;
-  });
+  const result = document.getElementById('result');
 
   btn.addEventListener('click', () => {
-   showPassword();
+    const results = ['大吉','中吉','凶','小吉'];
+    const n = Math.floor(Math.random() * results.length);
+
+    result.textContent = results[n];
+
   });
 
-  showPassword();
 }
